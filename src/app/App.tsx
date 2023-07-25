aac2109c868fca69f60770187a28b1b3879b063d
Leonardo <littledoge02@gmail.com>
2023-07-25 20:05:59 -0300

    Updating config files for github pages :wrench:

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ import Home from "./pages/home/Home";
 export default function App(): JSX.Element {
     return (
         <ChakraProvider theme={theme}>
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.DEV ? "/" : "/sudoku-app/"}>
                 <Routes>
                     <Route path="/" element={<Layout />}>
                         <Route index element={<Home />} />
